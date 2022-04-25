afa3e6936d7ccb8002fb1b9adece8c9114256be1
Rikard Olsson <rikard@thryselius.se>
2022-04-25 10:46:51 +0200

    More tests added

--- tests/test_maz.py
+++ tests/test_maz.py
@@ -1,5 +1,6 @@
 import maz
 import operator
+import functools
 
 def test_compose():
     inc = lambda x: x+1
@@ -28,3 +29,38 @@ def test_invoke_star():
 def test_sorted_pos():
     lst = [[2,1], [1,0], [3,0]]
     maz.sorted_pos(lst, operator.itemgetter(0)) == [[1,0], [2,1], [3,0]]
+
+def test_compose_pair():
+
+    def f(a, b):
+        return a+b
+
+    def g(x):
+        return x*3
+
+    a,b,x = 1,2,3
+    expected = (a+b)*x
+    composed = maz.compose_pair(g, f)
+    actual = composed(a,b)
+    assert actual == expected
+
+def test_compose():
+
+    part_mul = functools.partial(operator.mul, 3)
+    part_div = functools.partial(
+        operator.truediv,
+        2
+    )
+    composed = maz.compose(part_div, part_mul, operator.add)
+
+    expected = 1/6
+    actual = composed(2,2)
+    assert actual == expected
+
+def test_pospartial():
+
+    def f(x,y,z):
+        return x+y+z
+
+    _f = maz.pospartial(f, [(1, 32), (2, 19)])
+    assert _f(23) == 74
