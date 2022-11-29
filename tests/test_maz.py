227c8b23368e082ec37ba47493f2055fdf85578f
Rikard Olsson <rikard@thryselius.se>
2022-11-29 16:39:14 +0100

    More PEP8 fixes

--- tests/test_maz.py
+++ tests/test_maz.py
@@ -21,36 +21,41 @@ def test_compose():
 
 
 def test_pospartial():
-    
+  
     def add(x, y): 
         return x + y
-    
+   
     add_two = maz.pospartial(add, [(1, 2)])
     assert add_two(3) == 5
-    
+   
     def f(x, y, z):
         return x + y + z
 
     _f = maz.pospartial(f, [(1, 32), (2, 19)])
     assert _f(23) == 74
 
+
 def test_indexing():
     lst = [2, 1, 3]
     assert maz.indexing(lst, 0) == 2
+
 
 def test_invoke():
     def add(x, y):
         return x + y
     assert maz.invoke(add, [1, 2]) == 3
 
+
 def test_invoke_star():
     def add(x, y):
         return x + y
     assert maz.invoke_star(add, 1, 2) == 3
 
+
 def test_sorted_pos():
     lst = [[2, 1], [1, 0], [3, 0]]
     maz.sorted_pos(lst, operator.itemgetter(0)) == [[1, 0], [2, 1], [3, 0]]
+
 
 def test_compose_pair():
 
